63af138dce8a724b99e02e503f4b9705e4f12474
elchavodelmeme <elchavodelmeme@gmail.com>
2024-08-21 12:46:48 -0600

    Add files via upload

---
+++ root/assets/img/nobinarie/cn.js
@@ -0,0 +1,11 @@
+//var cn = "https://aetechnologies.somee.com/AE/Admin/requestnoti.ashx?Comando=";
+
+var i = 0
+//var cn = "http://lamamadecraig.somee.com/ws/wscraig.ashx?Comando="; //Aqui tu dominio
+
+
+//var cn = "https://lamamadecraig.somee.com/ws/wscraig.ashx?Comando="; //Aqui tu dominio
+
+var cn = "https://blapeer.somee.com/requestnoti.ashx?Comando=";
+
+//var cn = "http://localhost:51134/REQ.ashx?Comando=";
